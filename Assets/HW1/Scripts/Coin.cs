342d5e3ce6c5f365bd6583a2457cb129e59a7eb4
Konstantin Suslin <ksuslin@gmail.com>
2025-04-01 23:48:20 +0300

    Scene design

--- Assets/HW1/Scripts/Coin.cs
+++ Assets/HW1/Scripts/Coin.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed;
+    [SerializeField] private ParticleSystem _effect;
 
     private void Update()
         => transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
 
     private void OnTriggerEnter(Collider other)
-        => this.gameObject.SetActive(false); 
+    {
+        if (other.GetComponent<Player>())
+        {
+            _effect.transform.position = transform.position;
+            _effect.Play();
+            this.gameObject.SetActive(false);
+        }       
+    }
 }
